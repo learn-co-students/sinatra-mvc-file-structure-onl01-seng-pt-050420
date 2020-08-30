f628825d6a44c090c06444aeb65ff21311208bda
Jennifer Prince <jennifergraceprince@gmail.com>
2020-08-30 04:46:59 +0000

    Done.

---
+++ app/models/dogs.rb
@@ -0,0 +1,16 @@
+class Dog 
+  attr_accessor :name, :breed, :age 
+  Dogs = []
+  
+  def initialize(name, breed, age)
+   @name = name
+   @breed = breed
+   @age = age
+   DOGS << self
+  end
+  
+  def self.all 
+    DOGS 
+  end
+  
+end
